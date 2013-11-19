3d1491071b4ac8ecca4dea03e7eaf4668f28b1fe
Karl Palsson <karlp@remake.is>
2013-11-19 11:23:56 +0000

    Substantial improvements to stlink wireshark dissector
    COmmitting before I do any more overhauls

--- stlink2.ws.lua
+++ stlink2.ws.lua
@@ -23,6 +23,10 @@ local debug_command_funcs = {
 	[0x42] = "Get Trace Count"
 }
 
+local response_codes = {
+    [0x80] = "OK"
+}
+
 local f = stlinkv2_proto.fields
 f.f_tfunc = ProtoField.uint8("stlinkv2.tfunc", "Function", base.HEX, top_funcs)
 f.f_dfunc = ProtoField.uint8("stlinkv2.dcmd", "Debug Command", base.HEX, debug_command_funcs)
@@ -30,49 +34,119 @@ f.f_addr = ProtoField.uint32("stlinkv2.addr", "Address", base.HEX)
 f.f_value = ProtoField.uint32("stlinkv2.value", "Value", base.HEX)
 f.f_length = ProtoField.uint16("stlinkv2.length", "Length", base.DEC)
 f.f_data = ProtoField.bytes("stlinkv2.data", "data")
+f.f_response_status = ProtoField.uint16("stlinkv2.response.status", "status", base.HEX, response_codes)
 
+local f_usb_ep_num = Field.new("usb.endpoint_number.endpoint")
+
+local function getstring(fi)
+    local ok, val = pcall(tostring, fi)
+    if not ok then val = "(unknown)" end
+    return val
+end
+
+-- write32 doesn't have a response on the in endpoint, it tweaks decoding on the _out_ endpoint
+local responses = {
+    NOTSET = 1, READMEM32 = 2, WRITEDEBUG = 3, READDEBUG = 4,
+    WRITEMEM32 = 5 }
+    
+local expected = responses.NOTSET
 
 function stlinkv2_proto.dissector(buffer, pinfo, tree)
 	pinfo.cols["protocol"] = "STLinkv2"
 
-	-- don't try and decode inbound packets like this.
-	-- this doesn't work either :(
-	print(pinfo.dst)
-	if pinfo.dst == "host" then
-		return
-	end
+        local fields = { all_field_infos() }
+        for ix, finfo in ipairs(fields) do
+            print(string.format("ix=%d, finfo.name = %s, finfo.value=%s", ix, finfo.name, getstring(finfo)))
+        end
 
 	-- create protocol tree
 	local t_stlinkv2 = tree:add(stlinkv2_proto, buffer())
 	local offset = 0
 
-	local func_code = buffer(offset, 1):uint()
+        -- response data on general IN endpoint
+	local ep = f_usb_ep_num()
+        if (ep.value == 1) then
+                pinfo.cols["info"] = "Response"
+                if expected == responses.WRITEDEBUG then
+                    t_stlinkv2:add_le(f.f_response_status, buffer(offset, 2))
+                    offset = offset + 2
+                    pinfo.cols["info"]:append(" OK")
+                elseif expected == responses.READMEM32 then
+                    -- FIXME - we only handle decoding single word reads :(
+                    -- would need to save the count from the request?
+                    t_stlinkv2:add_le(f.f_value, buffer(offset, 4))
+                    offset = offset + 4
+                else
+	            t_stlinkv2:add(f.f_data, buffer(offset))
+                end
+		return
+	end
+
+        -- swo input data
+        if (ep.value == 3) then
+            pinfo.cols["info"] = "SWO/SWV data output"
+	    t_stlinkv2:add(f.f_data, buffer(offset))
+            return
+        end
+
+--[[
+        print ("expected is: " .. tostring(expected))
+        print ("responses.WRITEMEM32 is " .. tostring(responses.WRITEMEM32))
+        if (expected == responses.WRITEMEM32) then
+            assert(ep.value == 2)
+            -- FIXME - only works for single word writes!
+            local value = buffer(offset, 4):le_uint()
+            t_stlinkv2:add(f.value, value)
+            pinfo.cols["info"]:append(" Writemem32 data out")
+            return
+        end
+]]--
+
+	local func_code = buffer(offset, 1)
+        expected = responses.NOTSET
 	t_stlinkv2:add(f.f_tfunc, func_code)
+        func_code = func_code:uint()
 	offset = offset + 1
 	-- set info column to function name
 	pinfo.cols["info"] = top_funcs[func_code]
 
 	if func_code == 0xf2 then
-		tfunc = buffer(offset, 1):uint()
+		tfunc = buffer(offset, 1)
 		t_stlinkv2:add(f.f_dfunc, tfunc)
-		pinfo.cols["info"]:append(" - " .. debug_command_funcs[tfunc])
+                tfunc = tfunc:uint()
+		pinfo.cols["info"]:append(" - " .. tostring(debug_command_funcs[tfunc]))
 		offset = offset + 1
 		if tfunc == 0x35 then -- write debug reg
-			t_stlinkv2:add_le(f.f_addr, buffer(offset, 4))
-			t_stlinkv2:add_le(f.f_value, buffer(offset + 4, 4))
-			offset = offset + 8
+                        local addr = buffer(offset, 4)
+                        local value = buffer(offset + 4, 4)
+			t_stlinkv2:add_le(f.f_addr, addr)
+			t_stlinkv2:add_le(f.f_value, value)
+                        local extra = string.format(" %#x => %d (%#x)", addr:le_uint(), value:le_uint(), value:le_uint())
+                        pinfo.cols["info"]:append(extra)
+                        expected = responses.WRITEDEBUG
+                        offset = offset + 8
 		elseif tfunc ==  0x36 then -- read debug reg
-			t_stlinkv2:add_le(f.f_addr, buffer(offset, 4))
+                        local addr = buffer(offset, 4)
+			t_stlinkv2:add_le(f.f_addr, addr)
+                        pinfo.cols["info"]:append(string.format(" %#x", addr:le_uint()))
 			offset = offset + 4
+                        expected = responses.READDEBUG
 		elseif tfunc == 0x07 then -- readmem32
-			t_stlinkv2:add_le(f.f_addr, buffer(offset, 4))
-			t_stlinkv2:add_le(f.f_length, buffer(offset + 4, 2))
+                        local addr = buffer(offset, 4):le_uint()
+                        local length = buffer(offset + 4, 2):le_uint()
+			t_stlinkv2:add(f.f_addr, addr)
+			t_stlinkv2:add(f.f_length, length)
+                        pinfo.cols["info"]:append(string.format(" %#x @ %d", addr, length))
 			offset = offset + 6
+                        expected = responses.READMEM32
 		elseif tfunc == 0x08 then -- writemem32
-			t_stlinkv2:add_le(f.f_addr, buffer(offset, 4))
-			t_stlinkv2:add_le(f.f_length, buffer(offset + 4, 2))
+                        local addr = buffer(offset, 4):le_uint()
+                        local length = buffer(offset + 4, 2):le_uint()
+			t_stlinkv2:add(f.f_addr, addr)
+			t_stlinkv2:add(f.f_length, length)
+                        pinfo.cols["info"]:append(string.format(" %#x @ %d", addr, length))
 			offset = offset + 6
-			-- need to flag that the next write is the values...
+                        expected = responses.WRITEMEM32
 		end
 			
 	end
